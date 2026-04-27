90dea22eb9c3067a4a0bd335545e954bfa17b472
gohitx <gohit21@gmail.com>
2026-04-27 15:55:12 -0500

    🦄 chore: migrate to typescript-eslint and expand esbuild configuration to support multi-target webview builds

--- esbuild.js
+++ esbuild.js
@@ -4,32 +4,63 @@ const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
 
 /**
- * @type {import('esbuild').BuildOptions}
+ * @type {import('esbuild').Plugin}
  */
-const baseConfig = {
-  entryPoints: ["src/extension.ts"],
-  bundle: true,
-  outfile: "dist/extension.js",
-  external: ["vscode"],
-  format: "cjs",
-  platform: "node",
-  target: "node16",
-  sourcemap: !production,
-  minify: production,
-  logLevel: "info",
+const esbuildProblemMatcherPlugin = {
+	name: 'esbuild-problem-matcher',
+
+	setup(build) {
+		build.onStart(() => {
+			console.log('[watch] build started');
+		});
+		build.onEnd((result) => {
+			result.errors.forEach(({ text, location }) => {
+				console.error(`✘ [ERROR] ${text}`);
+				console.error(`    ${location.file}:${location.line}:${location.column}:`);
+			});
+			console.log('[watch] build finished');
+		});
+	},
 };
 
 async function main() {
-  const ctx = await esbuild.context(baseConfig);
-  if (watch) {
-    await ctx.watch();
-  } else {
-    await ctx.rebuild();
-    await ctx.dispose();
-  }
+	const ctxExtension = await esbuild.context({
+		entryPoints: ['src/extension.ts'],
+		bundle: true,
+		format: 'cjs',
+		minify: production,
+		sourcemap: !production,
+		sourcesContent: false,
+		platform: 'node',
+		outfile: 'dist/extension.js',
+		external: ['vscode', 'NeteaseCloudMusicApi'],
+		logLevel: 'silent',
+		plugins: [esbuildProblemMatcherPlugin],
+	});
+
+	const ctxWebview = await esbuild.context({
+		entryPoints: ['src/focus/view/ui/index.ts'],
+		bundle: true,
+		format: 'iife',
+		minify: production,
+		sourcemap: !production,
+		sourcesContent: false,
+		platform: 'browser',
+		outfile: 'dist/webview.js',
+		logLevel: 'silent',
+		plugins: [esbuildProblemMatcherPlugin],
+	});
+
+	if (watch) {
+		await Promise.all([ctxExtension.watch(), ctxWebview.watch()]);
+	} else {
+		await Promise.all([ctxExtension.rebuild(), ctxWebview.rebuild()]);
+		await ctxExtension.dispose();
+		await ctxWebview.dispose();
+	}
 }
 
 main().catch(e => {
-  console.error(e);
-  process.exit(1);
+	console.error(e);
+	process.exit(1);
 });
