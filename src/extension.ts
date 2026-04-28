5f2aae008b2413b22fa4bbb3ec90d532517ca0c1
gohitx <gohit21@gmail.com>
2026-04-27 22:14:04 -0500

    🛠️ refactor: remove SwapManager and simplify AI conversation keybindings

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,20 @@ import * as vscode from 'vscode';
 import { AICommandsManager } from './keymaps/ai-keymap-handler';
 import { ExtensionChecker } from './notifications/extension-checker';
 import { SmartWebviewExtension } from './notifications/smart-checker-webview';
-import { SwapManager } from './keymaps/swap';
+// import { SwapManager } from './keymaps/swap';
 
 let aiCommandsManagerInstance: AICommandsManager | undefined;
 let extensionCheckerInstance: ExtensionChecker | undefined;
 let smartWebviewExtensionInstance: SmartWebviewExtension | undefined;
-let swapManagerInstance: SwapManager | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
   aiCommandsManagerInstance = new AICommandsManager();
   extensionCheckerInstance = new ExtensionChecker();
   smartWebviewExtensionInstance = new SmartWebviewExtension();
-  swapManagerInstance = new SwapManager(aiCommandsManagerInstance);
 
   aiCommandsManagerInstance.registerCommands(context);
   extensionCheckerInstance.registerCheckCommands(context);
   smartWebviewExtensionInstance.registerWebviewCommands(context);
-  swapManagerInstance.initializeContexts();
 
   const checkF1QuickSwitchDisposable = vscode.commands.registerCommand(
     'lynx-keymap.checkF1QuickSwitch',
