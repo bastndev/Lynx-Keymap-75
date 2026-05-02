e5d1bd81d4c74c0e9125a9aeb2755673481a505c
gohitx <gohit21@gmail.com>
2026-05-02 15:28:13 -0500

    🧹 refactor: remove unused extension checker and webview notification utilities

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,30 @@
 import * as vscode from 'vscode';
 import { AICommandsManager, BottomTerminalManager, TerminalManager, AIToggleManager } from './keymaps';
-import { ExtensionChecker } from './notifications/extension-checker';
-import { SmartWebviewExtension } from './notifications/smart-checker-webview';
 
-let aiManager: AICommandsManager | undefined;
-let checkerManager: ExtensionChecker | undefined;
-let webviewManager: SmartWebviewExtension | undefined;
-let terminalManager: TerminalManager | undefined;
+let aiManager:             AICommandsManager     | undefined;
+let terminalManager:       TerminalManager       | undefined;
 let bottomTerminalManager: BottomTerminalManager | undefined;
-
-let aiToggleManager: AIToggleManager | undefined;
+let aiToggleManager:       AIToggleManager       | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
-  aiManager = new AICommandsManager();
-  checkerManager = new ExtensionChecker();
-  webviewManager = new SmartWebviewExtension();
-  terminalManager = new TerminalManager();
+  aiManager             = new AICommandsManager();
+  terminalManager       = new TerminalManager();
   bottomTerminalManager = new BottomTerminalManager();
-
-  aiToggleManager = new AIToggleManager();
+  aiToggleManager       = new AIToggleManager();
 
   aiManager.registerCommands(context);
-  checkerManager.registerCheckCommands(context);
-  webviewManager.registerWebviewCommands(context);
   terminalManager.registerCommands(context);
   bottomTerminalManager.registerCommands(context);
-
   aiToggleManager.registerCommands(context);
-  await context.workspaceState.update('lynx-keymap:lastActiveMode', undefined);
-  await context.workspaceState.update('lynx-keymap:originalTabsEnabled', undefined);
+
+  await context.workspaceState.update('lynx-keymap:lastActiveMode',          undefined);
+  await context.workspaceState.update('lynx-keymap:originalTabsEnabled',     undefined);
   await context.workspaceState.update('lynx-keymap:originalPanelShowLabels', undefined);
 }
 
 export async function deactivate() {
   aiManager?.dispose();
-  checkerManager?.dispose();
-  webviewManager?.dispose();
   terminalManager?.dispose();
   bottomTerminalManager?.dispose();
-
   aiToggleManager?.dispose();
 }
