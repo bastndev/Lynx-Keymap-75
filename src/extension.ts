890ec2a93fba04f8311324d3b1fe5f7975dd8f70
gohitx <gohit21@gmail.com>
2026-06-21 01:51:48 -0500

    refactor: update parameter name in activate function for clarity

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(_context: vscode.ExtensionContext) {
     vscode.window.showWarningMessage('⚠️ Lynx Keymap 75 ha sido fusionada con Lynx Keymap Pro. Esta extensión será eliminada pronto. Por favor, desinstálala.', 'Ver Lynx Keymap Pro').then(selection => {
         if (selection === 'Ver Lynx Keymap Pro') {
             vscode.env.openExternal(vscode.Uri.parse('vscode:extension/bastndev.lynx-keymap'));
